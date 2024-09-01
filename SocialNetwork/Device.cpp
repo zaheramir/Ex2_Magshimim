980927939a6f5e3cff838aadee5cc691ba22d12a
avihyb <avihyb@gmail.com>
2024-09-01 14:49:40 +0300

    Device class finished

---
+++ SocialNetwork/Device.cpp
@@ -0,0 +1,48 @@
+#include "Device.h"
+
+/*
+ * Device.cpp
+ * Implementation of the Device class.
+ */
+
+ // Init function: initializes the Device object with its parameters.
+void Device::init(unsigned int id, DeviceType type, const std::string& os)
+{
+    _id = id;
+    _type = type;
+    _os = os;
+    _isActive = true;
+}
+
+// Getters:
+    // getID() returns the ID of the Device object.
+unsigned int Device::getID() const {
+    return _id;
+}
+
+// getType() returns the type of the Device object.
+DeviceType Device::getType() const {
+    return _type;
+}
+
+// getOS() returns the OS of the Device object.
+const std::string& Device::getOS() const {
+    return _os;
+}
+
+// isActive() returns true if the Device object is active, false otherwise.
+bool Device::isActive() const {
+    return _isActive;
+}
+
+// activate() activates the Device object.
+
+void Device::activate() {
+	_isActive = true;
+}
+
+// deactivate() deactivates the Device object.
+
+void Device::deactivate() {
+	_isActive = false;
+}
---
+++ SocialNetwork/Device.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <string>
+
+///////////////////////////////////////////////////////////
+/*														 //
+    Those definitions are used for tests,                //
+    you can add your own, but please do not delete any.  //
+*/														 //
+enum DeviceType { PHONE, PC, LAPTOP, TABLET };		     //
+//
+#define UbuntuOS "UbuntuLinux"                           //						
+#define RedHatOS "RedHatLinux"							 //
+#define MacOS "MacOS"									 //
+#define IOS "IOS"										 //
+#define WINDOWS7 "Windows7"                              //
+#define WINDOWS10 "Windows10"                            //
+#define WINDOWS11 "Windows11"                            //
+#define ANDROID "Android"                                //
+                                                         //
+/// ///////////////////////////////////////////////////////
+
+
+class Device
+{
+public:
+    void init(unsigned int id, DeviceType type, const std::string& os);
+    unsigned int getID() const;
+    DeviceType getType() const;
+    const std::string& getOS() const;
+    bool isActive() const;
+    void activate();
+    void deactivate();
+
+private:
+    unsigned int _id;
+    DeviceType _type;
+    std::string _os;
+    bool _isActive;
+};
